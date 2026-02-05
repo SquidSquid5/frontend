37371acfee796b9b7ac24818921a2369bd0e69e8
za0012 <zinazeek@gmail.com>
2026-02-05 10:16:22 +0900

    feat: 약간의 스타일 변경, 로그인 함수 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,25 @@ function App() {
 	const loginId = useRef<HTMLInputElement>(null);
 	const loginPaw = useRef<HTMLInputElement>(null);
 
+	async function loginFn(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		try {
+			const res = await fetch("http://localhost:8080/api/users/login", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					email: loginId.current?.value,
+					password: loginPaw.current?.value,
+				}),
+			});
+			// const { data } = response;
+			window.location.href = "/home/";
+			return true;
+		} catch (error) {
+			console.log("로그인 에러!!", error);
+		}
+	}
+
 	return (
 		<div className="flex flex-col justify-center items-center min-h-screen bg-[linear-gradient(135deg,#667eea_0%,#764ba2_100%)] p-4">
 			<div className="mb-8 flex flex-col items-center text-white">
@@ -18,9 +37,9 @@ function App() {
 				<h1 className="text-4xl font-bold tracking-tight mb-2">ChatFlow</h1>
 				<p className="text-white/80 font-medium">실시간으로 소통하세요</p>
 			</div>
-			<div className="bg-white w-full max-w-[420px] rounded-[24px] shadow-2xl p-8">
+			<div className="bg-white w-full max-w-105 rounded-[24px] shadow-2xl p-8">
 				<h2 className="text-2xl font-bold text-gray-800 mb-5">로그인</h2>
-				<form className="flex flex-col">
+				<form className="flex flex-col" onSubmit={loginFn}>
 					<label className="mb-2 text-sm font-semibold text-gray-500 ml-1">
 						아이디
 					</label>
@@ -39,6 +58,7 @@ function App() {
 						placeholder="비밀번호를 입력하세요"
 						type="password"
 					/>
+					<p>test@example.com Test1234!</p>
 					<button
 						type="submit"
 						className="mt-4 w-full py-3 bg-[linear-gradient(90deg,#667eea_0%,#764ba2_100%)] text-white font-bold rounded-xl hover:opacity-90 transition-opacity"
@@ -46,18 +66,18 @@ function App() {
 						로그인
 					</button>
 				</form>
-				<div className="mt-8 flex flex-col items-center gap-6">
+				<div className="mt-5 flex flex-col items-center gap-3">
 					<button
 						type="button"
 						className="text-sm text-gray-400 hover:text-gray-600 transition-colors"
 					>
 						비밀번호를 잊으셨나요?
 					</button>
-					<div className="w-full pt-6 border-t border-gray-100 flex flex-col items-center gap-4">
+					<div className="w-full pt-4 border-t border-gray-100 flex flex-col items-center gap-4">
 						<p className="text-sm text-gray-400">아직 계정이 없으신가요?</p>
 						<button
 							type="button"
-							className="w-full py-3 border-2 border-indigo-500 text-indigo-500 font-bold rounded-xl hover:bg-indigo-50 transition-colors"
+							className="w-full py-2 border-2 border-indigo-500 text-indigo-500 font-bold rounded-xl hover:bg-indigo-50 transition-colors"
 						>
 							회원가입
 						</button>
